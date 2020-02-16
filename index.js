1014124169e045fa151c7ed1ebba541a51852272
youngkwon315 <39914288+youngkwon315@users.noreply.github.com>
2020-02-16 23:07:37 +0900

    20200216 Changes
    1. Installed babel(javascript compiler)

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
 const handleListening = () => {
-  console.log("Listening on: http://localhost:4000");
+  console.log(`Listening on: http://localhost:${PORT}`);
 };
+
+const handleHome = (req, res) => {
+  res.send("Hello");
+};
+const handleProfile = (req, res) => {
+  res.send("you are on my profile");
+};
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);

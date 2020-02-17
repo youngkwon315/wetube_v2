49ac282f5ee15c5274fac887608f4e9a8a027bb2
youngkwon315 <39914288+youngkwon315@users.noreply.github.com>
2020-02-17 23:46:16 +0900

    20200217 Changes for wetube v2

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -13,6 +18,12 @@ const handleHome = (req, res) => {
 const handleProfile = (req, res) => {
   res.send("you are on my profile");
 };
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
